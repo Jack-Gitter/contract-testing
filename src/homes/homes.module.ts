188e767409ad25059af3f16738cbd73a5c893ed3
Jack Gitter <jack.gitter@gmail.com>
2025-05-24 14:22:59 -0400

    nice

--- src/homes/homes.module.ts
+++ src/homes/homes.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HomesController } from './homes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Home } from './homes.entity';
+import { HomesService } from './homes.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Home])],
   controllers: [HomesController],
-  providers: [HomesModule],
+  providers: [HomesService],
 })
 export class HomesModule {}
