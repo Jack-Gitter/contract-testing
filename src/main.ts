ff9379e80f2c495a1ae5f2426038d95abe06cf5b
Jack Gitter <jack.gitter@gmail.com>
2025-05-24 15:00:09 -0400

    nice

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { OpenApiNestFactory } from 'nest-openapi-tools';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Contract Testing')
     .setDescription('Contract Testing Application')
     .setVersion('1.0')
-    .addTag('homes')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+    .addTag('homes');
 
-  app.useGlobalPipes(new ValidationPipe());
+  await OpenApiNestFactory.configure(app, config, {
+    fileGeneratorOptions: { enabled: true, outputFilePath: 'api-spec.yaml' },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
