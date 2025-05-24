e87d6d490f634790631092726748c03f9f6240ec
Jack Gitter <jack.gitter@gmail.com>
2025-05-24 14:04:33 -0400

    nice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { HomesModule } from './homes/homes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HomesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Calvinbean1!',
+      database: 'contract_testing',
+      migrationsRun: true,
+      entities: [],
+    }),
+    HomesModule,
+  ],
   controllers: [],
   providers: [],
 })
