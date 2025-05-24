4e31238442d00598c8a063e6db3f41cdd3a817b6
Jack Gitter <jack.gitter@gmail.com>
2025-05-24 14:52:12 -0400

    nice

--- src/homes/homes.dto.ts
+++ src/homes/homes.dto.ts
@@ -1,30 +1,23 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class FindHomeDTO {
   @IsString()
-  @ApiProperty()
   street: string;
 
   @IsString()
-  @ApiProperty()
   city: string;
 
   @IsString()
-  @ApiProperty()
   zip: string;
 }
 
 export class ReserveHomeDTO {
   @IsString()
-  @ApiProperty()
   street: string;
 
   @IsString()
-  @ApiProperty()
   city: string;
 
   @IsString()
-  @ApiProperty()
   zip: string;
 }
