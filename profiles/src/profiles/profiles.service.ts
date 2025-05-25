b20ff23f5d1cc9a06847363d2d9f380c6d0f6029
Jack Gitter <jack.gitter@gmail.com>
2025-05-25 12:46:30 -0400

    nice

--- profiles/src/profiles/profiles.service.ts
+++ profiles/src/profiles/profiles.service.ts
@@ -12,21 +12,19 @@ export class ProfilesService {
   names = ['someone1', 'someone2', 'someone3', 'someone4'];
 
   public async getProfile(id: number) {
-    try {
-      if (id >= this.names.length) {
-        throw new BadRequestException('no users found with that ID');
-      }
-      const url = `${this.homesServiceBaseUrl}/homes`;
-      const res = await firstValueFrom(this.httpService.get(url));
-      const homes: any[] = res.data;
-      const profileHomes = homes.filter((home) => {
-        return home.id === id;
-      });
+    if (id >= this.names.length) {
+      throw new BadRequestException('no users found with that ID');
+    }
+    const url = `${this.homesServiceBaseUrl}/homes`;
+    const res = await firstValueFrom(this.httpService.get(url));
+    const homes: any[] = res.data;
+    const profileHomes = homes.filter((home) => {
+      return home.id === id;
+    });
 
-      return {
-        name: this.names[id],
-        homes: profileHomes,
-      };
-    } 
+    return {
+      name: this.names[id],
+      homes: profileHomes,
+    };
   }
 }
