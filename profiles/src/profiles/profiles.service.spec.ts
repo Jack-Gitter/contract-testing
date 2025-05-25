58fbb020682cb09256553d44f214ffb7757835a4
Jack Gitter <jack.gitter@gmail.com>
2025-05-25 12:41:26 -0400

    nice

--- profiles/src/profiles/profiles.service.spec.ts
+++ profiles/src/profiles/profiles.service.spec.ts
@@ -2,28 +2,38 @@ import { Test } from '@nestjs/testing';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesService } from './profiles.service';
 import { execSync } from 'node:child_process';
+import { HttpModule, HttpService } from '@nestjs/axios';
 
 describe(ProfilesService.name, () => {
   let profileService: ProfilesService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [],
+      imports: [HttpModule],
       controllers: [ProfilesController],
-      providers: [ProfilesService],
+      providers: [
+        {
+          provide: ProfilesService,
+          useFactory: (httpService: HttpService) => {
+            return new ProfilesService(httpService, 'http://localhost:4010');
+          },
+          inject: [HttpService],
+        },
+      ],
     }).compile();
 
     profileService = moduleRef.get(ProfilesService);
 
-    execSync('npm run mock');
+    execSync('npm run mock up');
   });
 
   afterAll(async () => {
-      execSync('')
+    execSync('npm run mock down');
   });
 
-  beforeEach(async () => {}).compile();
-
-  afterEach(async () => {});
-
-  describe('Get Profile', () => {});
+  describe('Get Profile', () => {
+    it('properly integrates with the homes service', async () => {
+      const res = await profileService.getProfile(1);
+      expect(res).not.toBeNull();
+    });
+  });
 });
